e16e09b9e8bb5dd1cade2645e5aec269bc31f0a4
AllahBuksh <allahbuksh241@gmail.com>
2019-05-17 16:57:55 +0500

    Auto-commit

--- index.js
+++ index.js
@@ -3,3 +3,12 @@
 function add(a, b){
 	return a+b;
 }
+
+console.log(add(10, 12))
+
+var shell = require('shelljs');
+
+if (shell.exec('git commit -am "Auto-commit"').code !== 0) {
+  shell.echo('Error: Git commit failed');
+  shell.exit(1);
+}
