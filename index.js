ccc3855baac0c2cba1ca3a924048b30351aa3503
AllahBuksh <allahbuksh241@gmail.com>
2019-05-19 11:20:02 +0500

    Auto-commit

--- index.js
+++ index.js
@@ -12,23 +12,29 @@ if (shell.exec('git add index.js').code !== 0) {
   shell.echo('Error: Git commit failed');
   shell.exit(1);
 }
-/*
+
 if(shell.exec('git config --global user.email "allahbuksh241@gmail.com"')) {
-	if(shell.exec('git config --global user.name "AllahBuksh"')){;
+	if(shell.exec('git config --global user.name "AllahBuksh"')){
+		shell.exec('git commit -m "Auto-commit"');
+		shell.exec('git remote add origin https://github.com/AllahBuksh/Pub1');
+		shell.exec('git push origin master');
+		
 		shell.echo('User Configured');
 	}
-}*/
+}/**/
 
 /*if (shell.exec('git commit -m "Auto-commit"').code !== 0) {
   shell.echo('Error: Git commit failed');
   shell.exit(1);
-}*/
-
-/*if (shell.exec('git remote add origin https://github.com/AllahBuksh/Pub1').code !== 0) {
-  shell.echo('Error: Git commit failed');
-  shell.exit(1);
 }
 
+
+if (shell.exec('git remote add origin https://github.com/AllahBuksh/Pub1').code !== 0) {
+  shell.echo('Error: Git commit failed');
+  shell.exit(1);
+}*/
+
+/*
 if (shell.exec('git remote -v').code !== 0) {
   shell.echo('Error: Git commit failed');
   shell.exit(1);
