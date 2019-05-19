96851a3762b4b8b8e7df91891be5421253860576
AllahBuksh <allahbuksh241@gmail.com>
2019-05-19 11:14:14 +0500

    Auto-commit

--- index.js
+++ index.js
@@ -8,12 +8,33 @@ console.log(add(10, 12))
 
 var shell = require('shelljs');
 
-/*if (shell.exec('git add index.js').code !== 0) {
+if (shell.exec('git add index.js').code !== 0) {
+  shell.echo('Error: Git commit failed');
+  shell.exit(1);
+}
+/*
+if(shell.exec('git config --global user.email "allahbuksh241@gmail.com"')) {
+	if(shell.exec('git config --global user.name "AllahBuksh"')){;
+		shell.echo('User Configured');
+	}
+}*/
+
+/*if (shell.exec('git commit -m "Auto-commit"').code !== 0) {
   shell.echo('Error: Git commit failed');
   shell.exit(1);
 }*/
 
-if (shell.exec('git commit -am "Auto-commit"').code !== 0) {
+/*if (shell.exec('git remote add origin https://github.com/AllahBuksh/Pub1').code !== 0) {
   shell.echo('Error: Git commit failed');
   shell.exit(1);
 }
+
+if (shell.exec('git remote -v').code !== 0) {
+  shell.echo('Error: Git commit failed');
+  shell.exit(1);
+}
+
+if (shell.exec('git push origin master')) {
+  shell.echo('Error: Git commit failed');
+  shell.exit(1);
+}*/
